93c76c6f196563a22057b7d145299e61ebd4a63d
Alexander Borisov <lex.borisov@gmail.com>
2019-11-02 00:23:56 +0300

    Changed HTML examples for current API.

--- examples/lexbor/html/tokenizer/tag_attributes.c
+++ examples/lexbor/html/tokenizer/tag_attributes.c
@@ -35,6 +35,14 @@ token_callback(lxb_html_tokenizer_t *tkz, lxb_html_token_t *token, void *ctx)
         return token;
     }
 
+    if (token->tag_id == LXB_TAG__UNDEF) {
+        token->tag_id = lxb_html_token_tag_id_from_data(tag_heap, token);
+        if (token->tag_id == LXB_TAG__UNDEF) {
+            lxb_html_tokenizer_status_set(tkz, LXB_STATUS_ERROR);
+            return NULL;
+        }
+    }
+
     mraw = lxb_html_tokenizer_mraw(tkz);
 
     tag = lxb_tag_name_by_id(tag_heap, token->tag_id, NULL);
@@ -75,6 +83,7 @@ int
 main(int argc, const char *argv[])
 {
     lxb_status_t status;
+    lxb_tag_heap_t *tags;
     lxb_html_tokenizer_t *tkz;
 
     const lxb_char_t data[] = "<div id=one-id class=silent ref='some &copy; a'>"
@@ -90,6 +99,14 @@ main(int argc, const char *argv[])
     if (status != LXB_STATUS_OK) {
         FAILED("Failed to create tokenizer object");
     }
+
+    tags = lxb_tag_heap_create();
+    status = lxb_tag_heap_init(tags, 128);
+    if (status != LXB_STATUS_OK) {
+        FAILED("Failed to init tags");
+    }
+
+    lxb_html_tokenizer_tag_heap_set(tkz, tags);
 
     /* Without copying input buffer */
     lxb_html_tokenizer_opt_set(tkz, LXB_HTML_TOKENIZER_OPT_WO_COPY);
