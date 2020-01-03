27c59e62c4ea0a9b362c586d8af7da402d3dc4ab
Alexander <lex.borisov@gmail.com>
2020-01-03 13:03:22 +0300

    DOM: version bump.

--- source/lexbor/dom/base.h
+++ source/lexbor/dom/base.h
@@ -15,7 +15,7 @@ extern "C" {
 #include "lexbor/core/base.h"
 
 
-#define LXB_DOM_VERSION_MAJOR 0
+#define LXB_DOM_VERSION_MAJOR 1
 #define LXB_DOM_VERSION_MINOR 2
 #define LXB_DOM_VERSION_PATCH 1
 
