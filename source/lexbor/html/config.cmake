d2f5324f84b339443cac3402f04879d3cffb22b3
Alexander <lex.borisov@gmail.com>
2023-01-27 12:29:37 +0300

    HTML: added internal dependencies on Selectors and CSS.

--- source/lexbor/html/config.cmake
+++ source/lexbor/html/config.cmake
@@ -1,3 +1,3 @@
-set(DEPENDENCIES "core dom ns tag")
+set(DEPENDENCIES "core dom ns tag css selectors")
 set(DESCRIPTION "The module implemented by HTML specification.
 Includes functions for parsing HTML, build DOM tree and DOM/HTML serialization.")
