23d48652f067721011d935915ae59cd079c64782
Alexander Borisov <lex.borisov@gmail.com>
2024-11-16 12:09:52 +0300

    Encoding: version bump.

--- source/lexbor/encoding/base.h
+++ source/lexbor/encoding/base.h
@@ -16,7 +16,7 @@ extern "C" {
 
 
 #define LXB_ENCODING_VERSION_MAJOR 2
-#define LXB_ENCODING_VERSION_MINOR 1
+#define LXB_ENCODING_VERSION_MINOR 2
 #define LXB_ENCODING_VERSION_PATCH 0
 
 #define LXB_ENCODING_VERSION_STRING                                            \
