574c6cd6cd78645b817fd0247019ef9eccdc900e
Alexander Borisov <lex.borisov@gmail.com>
2025-04-19 17:11:04 +0300

    HTML: fixed attribute cloning for bad HTML.
    Restoring bad HTML triggers the adoption agency algorithm which clones some
    nodes. The problem is that when cloning nodes, the attributes of these nodes
    were not cloned, but simply assigned from the original.
    
    This could lead to memory problems if an attribute is removed (destroyed) from
    a cloned element.
    
    For example:
    <a target="x"><div></a>
    
    After parsing:
    <a target="x"></a><div><a target="x"></a>
    
    Previously, target="x" indicated the same memory in different elements.
    A new attribute was created, but due to a typo in the code, the existing
    attribute was assigned instead of the created one.
    
    Per report from @nigolaj on GitHub.
    
    This closes #273 issue on GitHub.

--- test/lexbor/html/other.c
+++ test/lexbor/html/other.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Alexander Borisov
+ * Copyright (C) 2022-2025 Alexander Borisov
  *
  * Author: Alexander Borisov <borisov@lexbor.com>
  */
@@ -7,6 +7,31 @@
 #include <lexbor/html/html.h>
 
 #include <unit/test.h>
+
+static lexbor_action_t
+remove_attrs(lxb_dom_node_t *node, void *ctx)
+{
+    lxb_dom_element_t *elem;
+    lxb_dom_attr_t *attr, *next;
+
+    if (node->type != LXB_DOM_NODE_TYPE_ELEMENT) {
+        return LEXBOR_ACTION_OK;
+    }
+
+    elem = lxb_dom_interface_element(node);
+    attr = lxb_dom_element_first_attribute(elem);
+
+    while (attr) {
+        next = lxb_dom_element_next_attribute(attr);
+
+        lxb_dom_attr_remove(attr);
+        lxb_dom_attr_interface_destroy(attr);
+
+        attr = next;
+    }
+
+    return LEXBOR_ACTION_OK;
+}
 
 TEST_BEGIN(fixed_svg_tags)
 {
@@ -37,12 +62,44 @@ TEST_BEGIN(fixed_svg_tags)
 }
 TEST_END
 
+TEST_BEGIN(bad_html_remove_attributes)
+{
+    lxb_status_t status;
+    lexbor_str_t str;
+    lxb_dom_node_t *root;
+    lxb_html_document_t *document;
+
+    static const lxb_char_t html[] = "<a target=\"x\"><div></a>";
+    static const lxb_char_t res[] = "<html><head></head><body><a></a>"
+                                    "<div><a></a></div></body></html>";
+
+    document = lxb_html_document_create();
+    test_ne(document, NULL);
+
+    status = lxb_html_document_parse(document, html, sizeof(html));
+    test_eq(status, LXB_STATUS_OK);
+
+    root = lxb_dom_document_root(lxb_dom_interface_document(document));
+
+    lxb_dom_node_simple_walk(root, remove_attrs, NULL);
+
+    str.data = NULL;
+    lxb_html_serialize_tree_str(root, &str);
+
+    test_eq(str.length, sizeof(res) - 1);
+    test_eq_str(str.data, res);
+
+    lxb_html_document_destroy(document);
+}
+TEST_END
+
 int
 main(int argc, const char * argv[])
 {
     TEST_INIT();
 
     TEST_ADD(fixed_svg_tags);
+    TEST_ADD(bad_html_remove_attributes);
 
     TEST_RUN("lexbor/html/other");
     TEST_RELEASE();
