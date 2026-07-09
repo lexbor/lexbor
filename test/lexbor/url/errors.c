5fc0ca71d3d1448c7f7fe7d09fe70fd322783611
Alexander Borisov <lex.borisov@gmail.com>
2026-07-10 01:09:22 +0300

    URL: added test for empty host after userinfo.

--- test/lexbor/url/errors.c
+++ test/lexbor/url/errors.c
@@ -8,6 +8,12 @@
 #include <lexbor/url/url.h>
 
 
+typedef struct {
+    const lxb_char_t *input;
+}
+empty_host_error_t;
+
+
 TEST_BEGIN(use_log_after_free)
 {
     lxb_status_t status;
@@ -29,12 +35,57 @@ TEST_BEGIN(use_log_after_free)
 }
 TEST_END
 
+TEST_BEGIN(empty_host_after_userinfo)
+{
+    size_t length;
+    lxb_status_t status;
+    lxb_url_parser_t parser;
+    lexbor_plog_entry_t *error;
+    static const empty_host_error_t entries[] = {
+        {(const lxb_char_t *) "https://user:pass@"},
+        {(const lxb_char_t *) "https://user@"},
+        {(const lxb_char_t *) "https://@"},
+        {(const lxb_char_t *) "https://user:pass@/"},
+        {(const lxb_char_t *) "https://user:pass@?x"},
+        {(const lxb_char_t *) "https://user:pass@#x"}
+    };
+
+    status = lxb_url_parser_init(&parser, NULL);
+    test_eq(status, LXB_STATUS_OK);
+
+    for (size_t i = 0; i < sizeof(entries) / sizeof(empty_host_error_t); i++) {
+        length = strlen((const char *) entries[i].input);
+
+        status = lxb_url_parse_basic(&parser, NULL, NULL, entries[i].input,
+                                     length, LXB_URL_STATE__UNDEF,
+                                     LXB_ENCODING_AUTO);
+        test_eq(status, LXB_STATUS_ERROR_UNEXPECTED_DATA);
+        test_ne(parser.log, NULL);
+        test_eq_size(lexbor_plog_length(parser.log), 2UL);
+
+        error = lexbor_array_obj_get(&parser.log->list, 0);
+        test_ne(error, NULL);
+        test_eq(error->id, LXB_URL_ERROR_TYPE_INVALID_CREDENTIALS);
+
+        error = lexbor_array_obj_get(&parser.log->list, 1);
+        test_ne(error, NULL);
+        test_eq(error->id, LXB_URL_ERROR_TYPE_HOST_MISSING);
+
+        lxb_url_parser_clean(&parser);
+    }
+
+    lxb_url_parser_memory_destroy(&parser);
+    lxb_url_parser_destroy(&parser, false);
+}
+TEST_END
+
 int
 main(int argc, const char * argv[])
 {
     TEST_INIT();
 
     TEST_ADD(use_log_after_free);
+    TEST_ADD(empty_host_after_userinfo);
 
     TEST_RUN("lexbor/url/errors");
     TEST_RELEASE();
