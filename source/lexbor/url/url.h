ddc444818cdcdd9c4faddf80648f20d9f4cfde24
Máté Kocsis <kocsismate@woohoolabs.com>
2026-08-07 21:33:13 +0200

    Expose lxb_url_ipv6_parse()
    This header is going to be used in https://github.com/php/php-src/pull/22268/

--- source/lexbor/url/url.h
+++ source/lexbor/url/url.h
@@ -197,6 +197,19 @@ typedef lexbor_action_t
 (*lxb_url_search_params_match_f)(lxb_url_search_params_t *sp,
                                  lxb_url_search_entry_t *entry, void *ctx);
 
+/*
+ * Parse an IPv6 address into its eight 16-bit pieces.
+ *
+ * @param[in] lxb_url_parser_t *. Cannot be NULL.
+ * @param[in] lxb_char_t *. Cannot be NULL.
+ * @param[in] lxb_char_t *. Cannot be NULL.
+ * @param[out] uint16_t *. Cannot be NULL. Output buffer for the eight parsed IPv6 pieces.
+ *
+ * @return LXB_STATUS_OK if successful, otherwise an error status value.
+ */
+lxb_status_t
+lxb_url_ipv6_parse(lxb_url_parser_t *parser, const lxb_char_t *data,
+                   const lxb_char_t *end, uint16_t *ipv6);
 
 /*
  * Create lxb_url_parser_t object.
