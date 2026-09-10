ad7b2f8b8315d9270bc476603ad9b104b7f06382
Tim Düsterhus <tim@tideways-gmbh.com>
2026-09-10 10:44:43 +0200

    URL: Fix parsing of query and fragment after a dot-component in path
    Fixes lexbor/lexbor#409

--- source/lexbor/url/url.c
+++ source/lexbor/url/url.c
@@ -2870,6 +2870,10 @@ lxb_url_path_try_dot(lxb_url_t *url, const lxb_char_t **begin,
         }
     }
 
+    if (((c == '?' || c == '#') && bqs)) {
+        p--;
+    }
+
     if (count == 0 || count > 2) {
         return LXB_STATUS_OK;
     }
